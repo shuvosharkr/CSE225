8593388173473b8ba8cd107f580ad5ce29d0d825
shuvosharkr <shuvoo0505@gmail.com>
2026-01-05 04:39:58 +0000

    Datatypes.cpp

---
+++ Datatypes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=5;
+    int* p=NULL;
+
+    cout<< *p <<endl;
+
+    // cout << x << " " << &x << " " << p << " " << *p << endl;
+    //  int** px= &p;
+
+    //  cout<< &p <<endl;
+    //  cout<< px <<endl;
+    //  cout<< *px <<endl;
+
+    return 0;
+}
