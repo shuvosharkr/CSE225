253329829d6a4adbca401330dc0b7034082bce39
shuvosharkr <shuvoo0505@gmail.com>
2026-01-09 04:39:58 +0000

    recur.cpp

---
+++ Coin/recur.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+float findSum(int n){
+    float sum=0;
+    if(n==1){
+        return 1;
+    }
+    else{
+    sum=sum+(n/(n-1))
+    return findsum((n-1)/n*(n-1);
+    }
+}
