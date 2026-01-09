d6ac002878a1634c8f31841853703c23265e90c7
shuvosharkr <shuvoo0505@gmail.com>
2026-01-09 04:39:58 +0000

    UnsortedType.h

---
+++ Assignment-1/UnsortedType.h
@@ -0,0 +1,30 @@
+#ifndef UNSORTEDTYPE_H
+#define UNSORTEDTYPE_H
+template <class ItemType>
+class UnsortedType
+{
+    struct NodeType
+    {
+        ItemType info;
+        NodeType* next;
+    };
+public:
+    UnsortedType();
+    ~UnsortedType();
+    bool IsFull();
+    int LengthIs();
+    void MakeEmpty();
+    void RetrieveItem(ItemType&, bool&);
+    void InsertItem(ItemType);
+    void DeleteItem(ItemType);
+    void ResetList();
+    void GetNextItem(ItemType&);
+    void PrintList();
+private:
+    NodeType* listData;
+    int length;
+    NodeType* currentPos;
+
+#endif // UNSORTEDTYPE_H
+};
+
