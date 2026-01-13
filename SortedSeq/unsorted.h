31fcaf6e44e7c519f976686140d5829be9e0653f
shuvosharkr <shuvoo0505@gmail.com>
2026-01-13 04:39:58 +0000

    unsorted.h

---
+++ SortedSeq/unsorted.h
@@ -0,0 +1,23 @@
+#ifndef UNSORTED_H_INCLUDED
+#define UNSORTED_H_INCLUDED
+const int max_item = 5;
+template<class T>
+class UnsortedType
+{
+private:
+    T data[max_item];
+    int length;
+    int currentPos;
+public:
+    UnsortedType();
+    bool isEmpty();
+    bool isFull();
+    void makeEmpty();
+    void insertItem(T);
+    void deleteItem(T);
+    int lengthIs();
+    void getNextItem(T&);
+    void resetList();
+    void retriveItem(T&, bool&);
+};
+#endif // UNSORTED_H_INCLUDED
